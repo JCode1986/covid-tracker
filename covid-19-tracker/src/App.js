07095500e7dc2c740a3c43147d1d541550a3afc0
JC <joseph.hangarter@yahoo.com>
2021-01-04 22:20:17 -0800

    api call from disease.sh successful for dropdown

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core"
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+      });
+    };
+    getCountriesData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_header">
+        <h1>Covid 19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+            {/* loop through countries for dropdown; api call from disease.sh */}
+            {
+              countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* header */}
+      {/* Title + select input dropdown field */}
+      
+      {/* info box 1 */}
+      {/* info box 2 */}
+      {/* info box 3 */}
+
+      {/* table */}
+      {/* graph */}
+
+      {/* map */}
     </div>
   );
 }
