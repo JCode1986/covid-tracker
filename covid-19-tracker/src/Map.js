58131013f0ce81938a5a730edbfa0de146ab447d
JC <joseph.hangarter@yahoo.com>
2021-01-08 23:24:48 -0800

    circle and popup on map now working

--- covid-19-tracker/src/Map.js
+++ covid-19-tracker/src/Map.js
@@ -1,5 +1,5 @@
 import React  from 'react'
-import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import './Map.css';
 import { showDataOnMap } from './util';
 
@@ -9,21 +9,25 @@ function Map({ countries, casesType, center, zoom }) {
         map.setView(center, zoom);
         return null;
     }
-    
+    console.log(countries, "countries")
+    console.log(casesType, "casesType")
     return (
-        <div className="map">
+        // <div className="map">
             <MapContainer 
+                casesType={casesType}
+                className="map"
                 center={center} 
                 zoom={zoom}
+                scrollWheelZoom={false}
             >
                 <ChangeView center={center} zoom={zoom} />
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />  
                 {showDataOnMap(countries, casesType)}
             </MapContainer>
-        </div>
+        // </div>
     );
 }
 
